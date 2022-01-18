d8a4bf6df3f93580d3c07418a96bf7cb66dfacf1
Ritesh Bucha <bucharitesh@gmail.com>
2022-01-18 20:59:52 +0530

    ✨  update config and exports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import * as Defaults from './AnalyticsService';
 
 export const { init } = Defaults;
+export const { tagManager } = Defaults;
+export const { adjustEvent } = Defaults;
+export const { adjust } = Defaults;
 
 export default {
   ...Defaults
